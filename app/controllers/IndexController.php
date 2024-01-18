cc69dba27e6fc1c9beccafbcb54650cdb7aee659
Matouš Suchý <suchy.matous@prumkadc.cz>
2024-01-18 21:23:46 +0100

    install

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,5 +6,18 @@ class IndexController
         echo \Template::instance()->render("index.php");
 
     }
+
+    public function install(\Base $base)
+    {
+        \models\applications::setdown();
+        \models\camps::setdown();
+        \models\kids::setdown();
+        \models\Users::setdown();
+
+        \models\applications::setup();
+        \models\camps::setup();
+        \models\kids::setup();
+        \models\Users::setup();
+    }
 }
 
